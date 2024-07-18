c6788a8d432e72e30f2d80488e4c9b1ef50689d0
marinafebiyola <marinafebiyol@gmail.com>
2024-07-18 15:45:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,25 +6,11 @@ import time
 st.set_page_config(layout="wide")
 
 # Judul Aplikasi
-st.title("MONITORING SYSTEM")
-
-# Menampilkan subheader di sudut kiri atas dan sudut kanan atas
-col1, col2 = st.columns(2)
-with col1:
-    st.subheader("BARELANG MARINE ROBOTICS TEAM")
-with col2:
-    st.subheader("POLITEKNIK NEGERI BATAM")
+st.title("Data Monitoring")
 
 # Fungsi untuk menghasilkan data random
 def generate_random_data():
-    return {
-        "DAY": "SUNDAY",  # Anda dapat mengganti ini dengan data dinamis sesuai kebutuhan
-        "DATE": random.randint(1, 10),
-        "TIME": random.randint(1, 10),
-        "COORDINATE": random.randint(1, 10),
-        "SOG": random.randint(1, 10),
-        "COG": random.randint(1, 10)
-    }
+    return random.randint(1, 10)
 
 # Kotak dengan border biru tua dan text box
 st.markdown("""
@@ -41,31 +27,16 @@ st.markdown("""
 placeholder = st.empty()
 
 # Loop untuk update data secara dinamis
+iteration = 0
 while True:
     data = generate_random_data()
+    unique_key = f"data_receive_{iteration}"  # Membuat kunci unik berdasarkan iterasi
     
     # Update konten dalam placeholder
-    with placeholder:
+    with placeholder.container():
         st.markdown('<div class="border-box">', unsafe_allow_html=True)
-        col1, col2, col3, col4, col5, col6 = st.columns(6)
-        with col1:
-            st.text("DAY")
-            st.text(data["DAY"])
-        with col2:
-            st.text("DATE")
-            st.text(str(data["DATE"]))
-        with col3:
-            st.text("TIME")
-            st.text(str(data["TIME"]))
-        with col4:
-            st.text("COORDINATE")
-            st.text(str(data["COORDINATE"]))
-        with col5:
-            st.text("SOG")
-            st.text(str(data["SOG"]))
-        with col6:
-            st.text("COG")
-            st.text(str(data["COG"]))
+        st.text_area("Data Receive", value=str(data), height=100, disabled=True, key=unique_key)
         st.markdown('</div>', unsafe_allow_html=True)
     
+    iteration += 1
     time.sleep(1)  # Interval update dalam detik
