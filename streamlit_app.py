3acb992a5d80a17fc0ad207773dfd1df1f7e6fa4
marinafebiyola <marinafebiyol@gmail.com>
2024-07-18 16:50:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,16 @@ backend_url = "http://103.20.185.106:5000/random"
 
 # Loop untuk update data secara dinamis
 while True:
-    response = requests.get(backend_url)
-    data = response.json()["data"]
-    
-    # Update konten dalam placeholder
-    with placeholder.container():
-        st.text_area("Data Receive", value=str(data), height=100, disabled=True, key="data_receive")
-    
-    time.sleep(1)  # Interval update dalam detik
+    try:
+        response = requests.get(backend_url)
+        data = response.json()["data"]
+
+        # Update konten dalam placeholder
+        with placeholder.container():
+            st.text_area("Data Receive", value=str(data), height=100, disabled=True, key="data_receive")
+
+        time.sleep(1)  # Interval update dalam detik
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")
+        break
