67cd02f621cdcc76e9167651c6539e9d7117fc17
marinafebiyola <marinafebiyol@gmail.com>
2024-07-25 16:07:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,20 @@
 import streamlit as st
-import requests
 
-# Ganti <ZeroTier_IP> dengan IP virtual ZeroTier dari perangkat yang menjalankan Flask
-backend_url = 'http://172.50.116.118:5000/data'
+# Judul besar
+st.title('Monitoring System')
 
-def fetch_data():
-    response = requests.get(backend_url)
-    return response.json()
+# Group box untuk menampilkan data sensor
+with st.beta_expander('Sensor Data', expanded=True):
+    # Membuat 4 textbox untuk menampilkan data sensor
+    sensor1 = st.text_input('Sensor 1 Data', value='0', key='sensor1')
+    sensor2 = st.text_input('Sensor 2 Data', value='0', key='sensor2')
+    sensor3 = st.text_input('Sensor 3 Data', value='0', key='sensor3')
+    sensor4 = st.text_input('Sensor 4 Data', value='0', key='sensor4')
 
-data = fetch_data()
-st.write('Data from Flask:', data['message'])
+    # Tombol untuk memperbarui data sensor (opsional)
+    if st.button('Update Data'):
+        st.write('Data updated')
+        # Anda bisa menambahkan kode di sini untuk mengambil data terbaru dari backend
+
+# Menambahkan informasi tambahan atau fitur lain di bawah group box
+st.write('Additional features can be added here')
