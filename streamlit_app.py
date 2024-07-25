1d7cc2af13a5dea198e18c633c82c1bd5f4a32fa
marinafebiyola <marinafebiyol@gmail.com>
2024-07-25 17:21:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,4 @@
 import streamlit as st
 import requests
 
-# URL LocalTunnel yang didapatkan dari langkah sebelumnya
-LOCAL_TUNNEL_URL = "https://quick-views-smash.loca.lt"  # Ganti dengan URL yang diberikan oleh LocalTunnel
-
 st.title("Monitoring System")
-
-# Buat permintaan ke backend Flask untuk mendapatkan data sensor
-response = requests.get(f"{LOCAL_TUNNEL_URL}/sensor_data")
-sensor_data = response.json()
-
-with st.expander("Sensor Data"):
-    st.write(f"Sensor 1: {sensor_data['sensor1']}")
-    st.write(f"Sensor 2: {sensor_data['sensor2']}")
-    st.write(f"Sensor 3: {sensor_data['sensor3']}")
-    st.write(f"Sensor 4: {sensor_data['sensor4']}")
