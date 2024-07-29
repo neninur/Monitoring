d02ba43f68a971cb735f83fa3d62ef0b644dd674
marinafebiyola <marinafebiyol@gmail.com>
2024-07-29 17:00:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,20 @@ import streamlit as st
 import requests
 
 # URL LocalTunnel yang didapatkan dari langkah sebelumnya
-LOCAL_TUNNEL_URL = "https://fancy-bats-wait.loca.lt"  # Ganti dengan URL yang diberikan oleh LocalTunnel
+LOCAL_TUNNEL_URL = "https://fine-pillows-listen.loca.lt"  # Ganti dengan URL yang diberikan oleh LocalTunnel
 
 st.title("Monitoring System")
 
 # Buat permintaan ke backend Flask untuk mendapatkan data sensor
-response = requests.get(f"{LOCAL_TUNNEL_URL}/sensor_data")
-sensor_data = response.json()
+try:
+    response = requests.get(f"{LOCAL_TUNNEL_URL}/sensor_data")
+    response.raise_for_status()  # Memeriksa apakah permintaan berhasil
+    sensor_data = response.json()
 
-with st.expander("Sensor Data"):
-    st.write(f"Sensor 1: {sensor_data['sensor1']}")
-    st.write(f"Sensor 2: {sensor_data['sensor2']}")
-    st.write(f"Sensor 3: {sensor_data['sensor3']}")
-    st.write(f"Sensor 4: {sensor_data['sensor4']}")
+    with st.expander("Sensor Data"):
+        st.write(f"Sensor 1: {sensor_data['sensor1']}")
+        st.write(f"Sensor 2: {sensor_data['sensor2']}")
+        st.write(f"Sensor 3: {sensor_data['sensor3']}")
+        st.write(f"Sensor 4: {sensor_data['sensor4']}")
+except requests.exceptions.RequestException as e:
+    st.error(f"Error fetching sensor data: {e}")
