7c007f14852262e221dcf6356e157d5d892c948f
neninur <neninur2508@gmail.com>
2024-07-31 12:57:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import requests
 st.title("Streamlit dengan Backend Flask")
 
 if st.button("Dapatkan Data Acak"):
-    response = requests.get("https://pretty-fans-double.loca.lt/random-data")
+    response = requests.get("https://twenty-eels-dress.loca.lt/random-data")
     data = response.json()
     st.write(f"Data Acak: {data['data']}")
